6f862fb18c8adfe46aa527bd0e9733da1350906f
lim <lim.ac96@gmail.com>
2019-12-31 02:42:37 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
+var path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "production",
+  // entry: {
+  //   app: ["./src/index.tsx"],
 
+  //   vendor: ["react", "react-dom"]
+  // },
+  // output: {
+  //   path: path.resolve(__dirname, "dist"),
+  //   publicPath: "/",
+  //   filename: "[name].js"
+  // },
   // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
 
+  devServer: {
+    // contentBase: path.join(__dirname, "dist"),
+    inline: false,
+    // compress: true,
+    historyApiFallback: true
+  },
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx"]
@@ -28,7 +45,12 @@ module.exports = {
       }
     ]
   },
-
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      filename: "index.html"
+    })
+  ],
   // When importing a module whose path matches one of the following, just
   // assume a corresponding global variable exists and use that instead.
   // This is important because it allows us to avoid bundling all of our
